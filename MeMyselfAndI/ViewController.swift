3f112bbc92aacef27a005596bf22e2099d77e3d5
aneeshack4 <aneesha@saimail.com>
2016-04-07 23:27:53 -0500

    Initial Commit

---
+++ MeMyselfAndI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeMyselfAndI
+//
+//  Created by Aneesha Kommineni on 4/7/16.
+//  Copyright © 2016 Aneesha Kommineni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
